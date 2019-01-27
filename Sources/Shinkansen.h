a4f2cca25c9b91ebbe71739bf9496f580e53e1b4
Simon Jarbrant <simonjarbrant@gmail.com>
2019-01-27 17:38:44 +0100

    Setup base project structure

---
+++ Sources/Shinkansen.h
@@ -0,0 +1,18 @@
+//
+//  Shinkansen.h
+//  Shinkansen
+//
+//  Created by Simon Jarbrant on 2019-01-27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Shinkansen.
+FOUNDATION_EXPORT double ShinkansenVersionNumber;
+
+//! Project version string for Shinkansen.
+FOUNDATION_EXPORT const unsigned char ShinkansenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shinkansen/PublicHeader.h>
+
+
