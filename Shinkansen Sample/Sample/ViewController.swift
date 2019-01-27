fbeeb9e2b54e7a547ffced86d9d3be76d1413878
Simon Jarbrant <simonjarbrant@gmail.com>
2019-01-27 17:51:24 +0100

    Add (empty) sample iOS app

---
+++ Shinkansen Sample/Sample/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Shinkansen Sample
+//
+//  Created by Simon Jarbrant on 2019-01-27.
+//  Copyright © 2019 Simon Jarbrant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
