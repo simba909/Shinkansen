47798e16eb1beda680a409584013daba41f95302
Simon Jarbrant <simonjarbrant@gmail.com>
2019-02-27 08:15:52 +0100

    Embed main ViewController in a UINavigationController

--- Shinkansen Sample/Sample/AppDelegate.swift
+++ Shinkansen Sample/Sample/AppDelegate.swift
@@ -11,11 +11,15 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
+    let window = UIWindow(frame: UIScreen.main.bounds)
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let mainViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: mainViewController)
+
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+
         return true
     }
 
@@ -40,7 +44,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
--- Shinkansen Sample/Sample/ViewController.swift
+++ Shinkansen Sample/Sample/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = "Sample"
+
         shinkansen.view = tableView
 
         let section = SimpleSection(values: ["Meh!", "Baaah..."])
